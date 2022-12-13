f6bce4f7c41c2f4c357e469d1af35daa911e24d5
Gabi Soda <garbrielpsoda@gmail.com>
2022-12-13 20:37:03 -0300

    Avanzado casi terminado, falta revisar

--- findNames.sh
+++ findNames.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 #Este es el tercer ejercicio del tp final de edp
+
+# Identificación de nombres propios (se identifican sólo si están en este formato
+# Nnnnnnnnn), aunque la palabra no sea un nombre propio realmente.
+# Ejemplos: Mateo, Estonoesunnombre, Ana.
+
+Nombres="^[A-Z]+[a-z]+[a-z]*"
+PALABRAS=$(cat $1 | tr "? ! : , ; ." "\n")
+for PALABRA in $PALABRAS;
+do
+	if [[ $PALABRA =~ $Nombres ]]; then
+		echo "Nombre:" "$PALABRA"
+	fi
+done
