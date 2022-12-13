6efc506d0ebda368a1b78ef681a41886ffc17b27
Gabi Soda <garbrielpsoda@gmail.com>
2022-12-13 20:02:56 -0300

    Terminado

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#Este es el segundo ejercicio del tp final de edp
+# Este es el segundo ejercicio del tp final de edp
+# Indicador estadístico de uso de palabras, deben ser de al menos 4(cuatro)
+# letras. (mostrar un Top Ten de estas palabras ordenadas desde la que tiene
+# más apariciones a la que tiene menos). Es decir, filtrar las palabras que
+# tengan al menos 4 letras y de éstas, elegir las 10 más usadas.
 
 for x in $(cat $1 | tr "? ! : , ; ." "\n" );
 do
@@ -11,11 +15,11 @@ do
 		echo "$x" >> palabras-ge4Letras.txt
 	fi
 done
-# "cat palabras-geLetras.txt" Mostramos el archivo en donde se guardaron las palabras,
+# "cat palabras-4.txt" Mostramos el archivo en donde se guardaron las palabras,
 # "sort" ordenamos las palabras de la más chica a la más grande
 # "uniq -c" agregamos al inicio de la línea el número de la cantidad de veces que aparecen
 # "sort -r" las ordenamos de mayor a menor pero ahora según el número de la cantidad de veces que aparecen
 # "head -n 10" mostramos las diez primeras
 
-cat palabras-geLetras.txt | sort | uniq -c | sort -r | head -n 10
-rm palabras-geLetras.txt
+cat palabras-4.txt | sort | uniq -c | sort -r | head -n 10
+rm palabras-4.txt
