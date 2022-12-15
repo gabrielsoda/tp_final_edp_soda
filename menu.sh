01b2b7336f0c40bdaa17500068dcfe7e837cd8bd
Gabi Soda <garbrielpsoda@gmail.com>
2022-12-14 23:56:00 -0300

    Menú terminado

--- menu.sh
+++ menu.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+
+echo "¿Qué herramienta quiere utilizar?"
+PS3='Elija >> '
+select ejercicio in "statsWords" "statsUsageWords" "findNames" "statsSentence" "blankLinesCounter" "SALIR"
+do
+	case $ejercicio in
+		statsWords)
+			source ./statsWords.sh
+			;;
+		statsUsageWords)
+			source ./statsUsageWords.sh
+			;;
+		findNames)
+			source ./findNames.sh
+			;;
+		statsSentence)
+			source ./statsSentence.sh
+			;;
+		blankLinesCounter)
+			source ./blankLinesCounter.sh
+			;;
+		SALIR)
+			break
+			;;
+		*)
+			echo "$REPLY no es una opción válida."
+			;;
+	esac
+done
